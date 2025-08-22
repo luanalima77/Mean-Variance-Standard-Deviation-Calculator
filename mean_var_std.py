6be668b866c39bafda8df9921610c948655b9b0f
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-08-22 08:58:53 -0300

    feat: adicionar cálculos das médias

--- mean_var_std.py
+++ mean_var_std.py
@@ -9,5 +9,10 @@ def calculate(list):
     matrix_3_x_3 = np.reshape(list, (3,3))
     flatten_matrix = matrix_3_x_3.flatten()
 
-
+    #OPERAÇÕES SOLICITADAS.
+    #Calculando as médias.
+    mean_axis_1 = np.mean(matrix_3_x_3, axis = 0).tolist() 
+    mean_axis_2 = np.mean(matrix_3_x_3, axis = 1).tolist() 
+    mean_flatten_matrix = np.mean(flatten_matrix).tolist()
+    
     return calculations
