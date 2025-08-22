563d1b3954f6a1c17808f7612b3cb59ca4f34b7e
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-08-22 08:57:49 -0300

    feat: converter lista em matriz 3x3 e gerar versão unidimensional (flatten)

--- mean_var_std.py
+++ mean_var_std.py
@@ -5,6 +5,9 @@ def calculate(list):
     if len(list) != 9:
         raise ValueError("List must contain nine numbers.")
 
+    #Convertendo a lista em matriz 3x3 e também transformando-a em unidimensional (flatten).
+    matrix_3_x_3 = np.reshape(list, (3,3))
+    flatten_matrix = matrix_3_x_3.flatten()
 
 
     return calculations
