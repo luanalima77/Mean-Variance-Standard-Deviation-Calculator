a64b5cc71d9ce87143aa3a69473438acd5e28749
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-08-22 08:55:34 -0300

    feat: adicionar verificação para garantir que a lista possua 9 elementos

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def calculate(list):
-
+    #Verificando se a lista fornecida possui quantidade de valores diferente de 9 e retornando um ValueError.
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
 
 
